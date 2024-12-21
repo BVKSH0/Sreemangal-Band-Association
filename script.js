ba3bdd993237539116701796caa568ea28390008
BVKSH0 <153601963+BVKSH0@users.noreply.github.com>
2024-12-21 11:33:58 +0600

    ADDED BANDS SLIDER

--- script.js
+++ script.js
@@ -5,4 +5,20 @@ function toggleMenu() {
   icon.classList.toggle("open");
 }
 
+// slider
+const productContainers = [...document.querySelectorAll(".bands-container")];
+const nxtBtn = [...document.querySelectorAll(".nxt-btn")];
+const preBtn = [...document.querySelectorAll(".pre-btn")];
 
+productContainers.forEach((item, i) => {
+  let containerDimensions = item.getBoundingClientRect();
+  let containerWidth = containerDimensions.width;
+
+  nxtBtn[i].addEventListener("click", () => {
+    item.scrollLeft += containerWidth;
+  });
+
+  preBtn[i].addEventListener("click", () => {
+    item.scrollLeft -= containerWidth;
+  });
+});
